ab7ea3e5daf8a58451a173483f99c4089a83c26f
eastor112 <emerar.mct@gmail.com>
2024-05-19 02:49:40 -0500

    [change] added dynamic height in map component

--- src/components/CarWashMap.vue
+++ src/components/CarWashMap.vue
@@ -1,14 +1,24 @@
 <script setup lang="ts">
-import { ref, watchEffect  } from 'vue'
+import { ref, watchEffect, onMounted, computed } from 'vue';
 
 // @ts-ignore
 import { initMap } from '@/lib/google-maps.ts'
 import { useUserStore } from '@/stores/user'
+import { useViewport } from '../composables/useViewport';
+import { useElementPosition } from '../composables/useElementPosition';
 
 const mapLoading = ref(true)
 
 const userStore = useUserStore()
+const { height } = useViewport()
+const { divRef, y } = useElementPosition()
 const mapCenterCoordinates = userStore.coordinates
+const dynamicHeight = ref('430px');
+
+watchEffect(() => {
+  const calculatedHeight = height.value - y.value - 30
+  dynamicHeight.value = `${calculatedHeight > 200? calculatedHeight: 200}px`
+})
 
 watchEffect(async () => {
   if (mapCenterCoordinates.latitude && mapCenterCoordinates.longitude) {
@@ -19,11 +29,19 @@ watchEffect(async () => {
     mapLoading.value = false
   }
 })
+
+const dinamicHeightStyles = computed(() => ({
+  height: dynamicHeight.value,
+}));
+
 </script>
 
 <template>
-  <div class="px-5">
-    <div v-if="mapLoading" class="relative flex justify-center items-center w-full h-96 text-lg font-bold text-gray-600">Cargando el mapa...</div>
-    <div id="map" class="h-[400px]"></div>
+  <div ref='divRef' class='test'>
+    <div v-if="mapLoading"
+      class="relative flex justify-center items-center w-full text-lg font-bold text-gray-600" :style="dinamicHeightStyles">
+      Cargando el mapa...
+    </div>
+    <div id="map" :style="dinamicHeightStyles"></div>
   </div>
 </template>
